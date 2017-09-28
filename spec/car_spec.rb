000e8cf70703ea2532a3316e5d464f1ed357b353
chemturion <aaron.k.kelton@gmail.com>
2017-09-28 17:30:02 -0500

    described Car#full_name with and without args

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -35,11 +35,23 @@ describe("Car") do
       c = %w(blue black red green)
       expect(Car.colors).to match_array(c)
     end
-    
+
   end
 
   describe "#full_name" do
-    #
+
+    it "returns a string in the expected format" do
+      @honda = Car.new(make: 'Honda', year: 2004, color: 'blue')
+      expect(@honda.full_name).to eq('2004 Honda (blue)')
+    end
+
+    context "when initialized with no arguments" do
+      it "returns a string using default values" do
+        @car = Car.new
+        expect(@car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+
   end
 
 end
