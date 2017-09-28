fb487fcb8e10d348b84dce6296fa95e4b91ce8cd
chemturion <aaron.k.kelton@gmail.com>
2017-09-28 17:21:00 -0500

    described Car attrs spec; one with explicit parens

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,17 +1,40 @@
 require 'car'
 
-describe "Car" do
+describe("Car") do
 
-  describe "attributes" do
+  describe("attributes") do
+
+    it("allows reading and writing for :make") do
+      car = Car.new
+      car.make=('Test')
+      expect(car.make).to(eq('Test'))
+    end
+
+    it "allows reading and writing for :year" do
+      car = Car.new
+      car.year = 9999
+      expect(car.year).to eq(9999)
+    end
+
+    it "allows reading and writing for :color" do
+      car = Car.new
+      car.color = 'red'
+      expect(car.color).to eq('red')
+    end
+
+    it "allows reading for :wheels" do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
 
   end
 
   describe ".colors" do
-
+    #
   end
 
   describe "#full_name" do
-
+    #
   end
 
 end
