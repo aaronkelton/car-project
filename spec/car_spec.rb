71093013d6da5770da7e5e8e1a402aca81a9ec82
chemturion <aaron.k.kelton@gmail.com>
2017-09-29 11:28:34 -0500

    created Truck class and abstracted the attributes tests to shared examples

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,45 +1,9 @@
 require 'car'
+require 'shared_examples/a_standard_vehicle'
 
 describe(Car) do
 
-  describe("attributes") do
-
-    # "subject" is implicitly available when
-    # using the classname Car instead of 'Car' string
-    # subject { Car.new }
-
-    # use "subject" instead of "let()"
-    # if variable is subject of example
-    # let(:car) { Car.new }
-
-    # "let" is better than "before" for
-    # setting up instance variables
-    # before(:example) do
-    #   @car = Car.new
-    # end
-
-    it("allows reading and writing for :make") do
-      subject.make=('Test')
-      expect(subject.make).to(eq('Test'))
-    end
-
-    it "allows reading and writing for :year" do
-      subject.year = 9999
-      expect(subject.year).to eq(9999)
-      # (subject.year).should eq(9999)
-    end
-
-    it "allows reading and writing for :color" do
-      subject.color = 'red'
-      expect(subject.color).to eq('red')
-    end
-
-    it "allows reading for :wheels" do
-      expect(subject.wheels).to eq(4)
-    end
-
-    it "allows writing for :doors"
-  end
+  it_behaves_like('a standard vehicle')
 
   describe ".colors" do
 
